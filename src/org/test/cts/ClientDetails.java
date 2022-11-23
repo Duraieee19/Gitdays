6d20d125a3933cf26e824cfd8c1bb1e4285106cd
Durai@123 <englishmani1998@gmail.com>
2022-11-23 08:01:17 +0530

    newwwwww

--- src/org/test/cts/ClientDetails.java
+++ src/org/test/cts/ClientDetails.java
@@ -1,21 +1,21 @@
 package org.test.cts;
 
-public class ClientDetails extends CompanyDetails{
+public class ClientDetails extends CompanyDetails {
 	public void clientId() {
 
-	System.out.println("Client Id Is 333");
+		System.out.println("Client Id Is 333");
 	}
+
 	public static void main(String[] args) {
 		ClientDetails c = new ClientDetails();
 		c.employeeId();
 		c.companyName();
 		c.companyId();
 		c.empAge();
-<<<<<<< HEAD
+
 		System.out.println("for conflict");
-=======
+
 		System.out.println("for GIT");
->>>>>>> aeb7234bffa7f8ead0b3a48318dd63325ea28612
-	
+
 	}
 }
